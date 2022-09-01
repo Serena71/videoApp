dc0e63a20600507e0f5a47615a2f98200d4aa3fd
Serena71 <serena991031zs@gmail.com>
2022-09-01 23:37:35 +1000

    add navbar component

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import styled from 'styled-components';
+import { LoginButton } from './LoginButton';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+
+const Container = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  color: ${({ theme }) => theme.text};
+  font-size: 14px;
+  position: sticky;
+  top: 0;
+  height: 56px;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  height: 100%;
+  padding: 0px 20px;
+  position: relative;
+`;
+
+const Search = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 40%;
+  border: 1px solid #aaaaaa;
+  border-radius: 5px;
+  position: absolute;
+  left: 0;
+  right: 0;
+  margin: auto;
+`;
+
+const Input = styled.input`
+  border: none;
+  background-color: transparent;
+  margin-left: 10px;
+  width: 90%;
+`;
+
+const Navbar = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Search>
+          <Input placeholder="Search"></Input>
+          <SearchOutlinedIcon />
+        </Search>
+        <LoginButton />
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Navbar;
